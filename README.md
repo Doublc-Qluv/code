d6071269990c6f32740e768da3ab018aaf39df59
Doublv-Qluv <963880465@qq.com>
2018-05-01 10:32:00 +0800

    code

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# 2018.4.23 信号与系统上机实验
-mablab代码及图片
-
+2018.4.23 add
+# matlab-signal-processing
+ 信号与系统上机实验 mablab代码及图片
--- python/001_gametest.py
+++
@@ -1,9 +0,0 @@
-print('----------START----------')
-temp = input("Guess what number is thought:")
-guess = int(temp)
-if guess == 8:
-	print("Yep,you are right!")
-	print("But no reward")
-else:
-	print("Wrong! the number is 8!")
-print("Bye")
